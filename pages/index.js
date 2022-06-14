d214a8fc6d56b9a298cf95afbe363684ebf020f1
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-06-13 20:42:07 -0400

    Update index.js
    Adicionada Barra de Busca

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,34 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react';
 
-const pessoas = require('./api/api.json');
+
 
 
 
 export default function Home() {
+
+const pessoas = require('./api/api.json');
+const [busca, setBusca] = useState('');
+
+
+const filtraBusca = pessoas.filter(({nome}) => nome.toLowerCase().startsWith(busca.toLowerCase()))
+
 return(
 
   <div>
-<head><title>Catálogo</title></head>
+<Head><title>Catálogo</title></Head>
 
 <div className="container">
 <h1>Usuários</h1>
-{pessoas.map(function(pessoas) {
+<input type="text" placeholder='Busque uma pessoa' onChange={(ev) => setBusca(ev.target.value)}/>
+{filtraBusca.map(function(pessoas) {
+
 const link = ("posts/" + pessoas.id )
   return(
 <div key={pessoas.id}>
- <a href={link}><p>{pessoas.nome} {pessoas.sobrenome}</p></a>
+ <a href={link}><div>{pessoas.nome} {pessoas.sobrenome}</div></a>
  
 </div>
 
